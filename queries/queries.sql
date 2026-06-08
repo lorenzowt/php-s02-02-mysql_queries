5a2fdbe7365f1b846c6b4a5c692a5ce2af061468
Lorenzo Marco <lorenzomfw@gmail.com>
2026-06-08 10:16:11 +0200

    Fix table name in SQL query for products

--- queries/queries.sql
+++ queries/queries.sql
@@ -1,7 +1,7 @@
 -- 1. Llista el nom de tots els productes que hi ha en la taula producto.
 SELECT nombre
 
-FROM productos;
+FROM producto;
 
 -- 2. Llista els noms i els preus de tots els productes de la taula producto.
 
