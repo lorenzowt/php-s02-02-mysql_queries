4067e0d045a52b18a44aed32a80b6e4f4f92fe42
Lorenzo Marco <lorenzomfw@gmail.com>
2026-06-08 10:14:44 +0200

    Add SQL queries to list product names

--- queries/queries.sql
+++ queries/queries.sql
@@ -1,5 +1,7 @@
 -- 1. Llista el nom de tots els productes que hi ha en la taula producto.
+SELECT nombre
 
+FROM productos;
 
 -- 2. Llista els noms i els preus de tots els productes de la taula producto.
 
